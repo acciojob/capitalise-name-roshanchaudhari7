13f9eeb08d770d9c70b74b9227cebd3b966f668d
Roshan Chaudhari <98824802+roshanchaudhari7@users.noreply.github.com>
2024-07-17 15:22:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const inputText = document.getElementById('fname');
+
+inputText.addEventListener('blur', function() {
+  this.value = this.value.toUpperCase();
+});
